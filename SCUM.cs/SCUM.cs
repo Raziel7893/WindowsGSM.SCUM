741cd62fef73de24e9df493a5845c73243220c5c
Alexander Barthel <159198554+Raziel7893@users.noreply.github.com>
2025-07-18 17:05:37 +0200

    added queryport as suggested in discord
    https://discord.com/channels/590590698907107340/604057263610527798/1395776503945363456

--- SCUM.cs/SCUM.cs
+++ SCUM.cs/SCUM.cs
@@ -44,7 +44,7 @@ namespace WindowsGSM.Plugins
 
         // - Game server default values
         public string Port = "10000"; // Default port
-        public string QueryPort = "10000"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+        public string QueryPort = "10002"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
         // TODO: Unsupported option
         public string Defaultmap = "Dedicated"; // Default map name
 
@@ -71,6 +71,7 @@ namespace WindowsGSM.Plugins
             // Prepare start parameter
             string param = $" {_serverData.ServerParam} ";
             param += $"-port={_serverData.ServerPort} ";
+            param += $"-QueryPort={_serverData.QueryPort} ";
             param += $"-MaxPlayers={_serverData.ServerMaxPlayer} ";
 
             Process p;
